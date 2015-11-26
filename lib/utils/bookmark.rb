6be5f6b87df9c1b23e9c684f548e50f13f2fa784
suiyujin <suiyujin04@gmail.com>
2015-11-26 15:36:20 +0900

    make bookmark.rb and page.rb

---
+++ lib/utils/bookmark.rb
@@ -0,0 +1,15 @@
+class Utils::Bookmark
+  attr_reader :timestamp, :comment, :user, :tags, :normalize_comment
+
+  def initialize(timestamp:, comment:, user:, tags: [])
+    @timestamp = timestamp
+    @comment = comment
+    @user = user
+    @tags = tags
+  end
+
+  private
+
+  def normalize_comment
+  end
+end
