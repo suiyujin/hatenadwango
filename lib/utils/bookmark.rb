f5640fed9ad53677961440f17e9c881b55afa327
suiyujin <suiyujin04@gmail.com>
2015-11-26 17:48:45 +0900

    normalize comment before parse

--- lib/utils/bookmark.rb
+++ lib/utils/bookmark.rb
@@ -7,8 +7,9 @@ class Utils::Bookmark
     @user = user
     @tags = tags
 
+    @normalize_comment = normalize_comment
     parser = CaboCha::Parser.new
-    @tree = parser.parse(comment)
+    @tree = parser.parse(@normalize_comment)
   end
 
   def make_base_array
@@ -18,5 +19,34 @@ class Utils::Bookmark
   private
 
   def normalize_comment
+    comment = @comment
+    comment.tr!("０-９Ａ-Ｚａ-ｚ", "0-9A-Za-z")
+    comment = Moji.han_to_zen(comment, Moji::HAN_KATA)
+    hypon_reg = /(?:˗|֊|‐|‑|‒|–|⁃|⁻|₋|−)/
+    comment.gsub!(hypon_reg, "-")
+    choon_reg = /(?:﹣|－|ｰ|—|―|─|━)/
+    comment.gsub!(choon_reg, "ー")
+    chil_reg = /(?:~|∼|∾|〜|〰|～)/
+    comment.gsub!(chil_reg, '')
+    comment.gsub!(/[ー]+/, "ー")
+    comment.tr!(%q{!"#$%&'()*+,-.\/:;<=>?@[¥]^_`{|}~｡､･｢｣"}, %q{！”＃＄％＆’（）＊＋，−．／：；＜＝＞？＠［￥］＾＿｀｛｜｝〜。、・「」})
+    comment.gsub!(/　/, " ")
+    comment.gsub!(/ {1,}/, " ")
+    comment.gsub!(/^[ ]+(.+?)$/, "\\1")
+    comment.gsub!(/^(.+?)[ ]+$/, "\\1")
+    while comment =~ %r{([\p{InCjkUnifiedIdeographs}\p{InHiragana}\p{InKatakana}\p{InHalfwidthAndFullwidthForms}\p{InCJKSymbolsAndPunctuation}]+?)[ ]{1}([\p{InCjkUnifiedIdeographs}\p{InHiragana}\p{InKatakana}\p{InHalfwidthAndFullwidthForms}\p{InCJKSymbolsAndPunctuation}]+?)}
+      comment.gsub!( %r{([\p{InCJKUnifiedIdeographs}\p{InHiragana}\p{InKatakana}\p{InHalfwidthAndFullwidthForms}\p{InCJKSymbolsAndPunctuation}]+?)[ ]{1}([\p{InCJKUnifiedIdeographs}\p{InHiragana}\p{InKatakana}\p{InHalfwidthAndFullwidthForms}\p{InCJKSymbolsAndPunctuation}]+?)}, "\\1\\2")
+    end
+    while comment =~ %r{([\p{InBasicLatin}]+)[ ]{1}([\p{InCJKUnifiedIdeographs}\p{InHiragana}\p{InKatakana}\p{InHalfwidthAndFullwidthForms}\p{InCJKSymbolsAndPunctuation}]+)}
+      comment.gsub!(%r{([\p{InBasicLatin}]+)[ ]{1}([\p{InCJKUnifiedIdeographs}\p{InHiragana}\p{InKatakana}\p{InHalfwidthAndFullwidthForms}\p{InCJKSymbolsAndPunctuation}]+)}, "\\1\\2")
+    end
+    while comment =~ %r{([\p{InCJKUnifiedIdeographs}\p{InHiragana}\p{InKatakana}\p{InHalfwidthAndFullwidthForms}\p{InCJKSymbolsAndPunctuation}]+)[ ]{1}([\p{InBasicLatin}]+)}
+      comment.gsub!(%r{([\p{InCJKUnifiedIdeographs}\p{InHiragana}\p{InKatakana}\p{InHalfwidthAndFullwidthForms}\p{InCJKSymbolsAndPunctuation}]+)[ ]{1}([\p{InBasicLatin}]+)}, "\\1\\2")
+    end
+    comment.tr!(
+      %q{！”＃＄％＆’（）＊＋，−．／：；＜＞？＠［￥］＾＿｀｛｜｝〜},
+      %q{!"#$%&'()*+,-.\/:;<>?@[¥]^_`{|}~}
+    )
+    comment
   end
 end
